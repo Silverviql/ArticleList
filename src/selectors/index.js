40e5151e78f9e45c409cdfac2ef3082289fbca8b
Silverviql <khabibulinemil@gmail.com>
2018-08-16 16:11:03 +0300

    add simple selector

---
+++ src/selectors/index.js
@@ -0,0 +1,19 @@
+import { createSelector } from  'reselect'
+
+
+//простые селекторы (достают данные из store)
+const filtersGetter = state => state.filters
+const articlesGetter = state => state.articles
+
+export const filtratedArticlesSelector = createSelector(articlesGetter, filtersGetter, (articles, filters) => {
+    const {selected, dateRange: {from, to}} = filters
+
+    console.log('---', 'recomputing filttration')
+
+    return  articles.filter(article => {
+        const published = Date.parse(article.date)
+        const lol = selected.map(select=> select.value)
+        return (!selected.length || lol.includes(article.id)) &&
+            (!from || !to || (published > from && published < to))
+    })
+})
